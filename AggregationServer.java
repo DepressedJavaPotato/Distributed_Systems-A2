aad6ca45cb1ee0fd7ae950fd0d02149425fb6c06
Maaz Arshad Beg <maazarshad.beg@student.adelaide.edu.au>
2024-09-24 00:52:13 +0930

    init AggregationServer

--- AggregationServer.java
+++ AggregationServer.java
@@ -1,5 +1,8 @@
+import java.util.Map;
 public class AggregationServer {
-
+    private Map<String, WeatherData> dataStore;
+    private LamportClock lamportClock;
+    private int port;
 }
 
 //functions
